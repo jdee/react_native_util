93f28ab5a96246ee7a6a96871469bcd300b5e663
Jimmy Dee <jgvdthree@gmail.com>
2019-05-10 09:13:05 -0700

    Added a TODO comment

--- lib/react_native_util/core_ext/regexp.rb
+++ lib/react_native_util/core_ext/regexp.rb
@@ -1,6 +1,9 @@
 class Regexp
   unless defined?(match?)
     # For portability to system Ruby
+    # TODO: Correct this. The condition above is meaningless.
+    # We're always overriding this method, even if it already
+    # exists. This is fine for now, but should be improved.
     def match?(str)
       !match(str).nil?
     end
