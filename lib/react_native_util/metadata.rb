20c1c031ba8ab9ce0f626df72928b078520af781
Jimmy Dee <jgvdthree@gmail.com>
2019-05-10 14:09:50 -0700

    Version bump to 0.1.3

--- lib/react_native_util/metadata.rb
+++ lib/react_native_util/metadata.rb
@@ -1,7 +1,7 @@
 # Conversion tools for React Native projects
 module ReactNativeUtil
   NAME = 'react_native_util'
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
   SUMMARY = 'Community utility CLI for React Native projects'
   DESCRIPTION = 'Automatically modifies a project created with react-native ' \
     'init to use the React pod from node_modules.'
