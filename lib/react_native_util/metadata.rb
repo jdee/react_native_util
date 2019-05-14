41ef858a320eda94d7a349f926e807f72b060290
Jimmy Dee <jgvdthree@gmail.com>
2019-05-14 12:19:11 -0700

    Version bump to 0.4.0

--- lib/react_native_util/metadata.rb
+++ lib/react_native_util/metadata.rb
@@ -1,7 +1,7 @@
 # Conversion tools for React Native projects
 module ReactNativeUtil
   NAME = 'react_native_util'
-  VERSION = '0.3.3'
+  VERSION = '0.4.0'
   SUMMARY = 'Community utility CLI for React Native projects'
   DESCRIPTION = 'Converts a project created with react-native init to use CocoaPods with the ' \
                 'React pod from node_modules. This preserves compatibility with ' \
